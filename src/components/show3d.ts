f4c41f56200fea3f34dee174a33a11293df3d53f
Ebrahim Ali <1ms19cs043@gmail.com>
2023-12-27 21:27:31 +0530

    first commit

--- src/components/show3d.ts
+++ src/components/show3d.ts
@@ -1,4 +1,5 @@
-import { setupViewer } from "./midryaish";
+import { setupViewer } from "./Viewer";
+
 
 export default function show3d() {
     let temp = document.createElement("template");
@@ -18,7 +19,7 @@ export default function show3d() {
   
     const show3dComponent = temp.content.firstElementChild as HTMLElement;
     const canvas= show3dComponent.querySelector('#webGI-canvas') as HTMLCanvasElement
-    setupViewer(canvas);
+    setupViewer(canvas)
   //close the 3d div
     const closeButton = show3dComponent.querySelector('.close-button') as HTMLElement;
     closeButton.addEventListener('click', () => {
